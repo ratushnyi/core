36471dc9c11621d86aeb65f355be65a8d0efc67d
yevheniira-MA <yevhenii.ra@moonactive.com>
2025-06-04 21:09:29 +0300

    Improve services

--- Core/Assets/Scripts/Services/ServiceBase.cs
+++ Core/Assets/Scripts/Services/ServiceBase.cs
@@ -1,5 +1,6 @@
 using System;
 using UniRx;
+using UnityEngine;
 using Zenject;
 
 namespace TendedTarsier.Core.Services
@@ -7,11 +8,26 @@ namespace TendedTarsier.Core.Services
     public abstract class ServiceBase : IDisposable
     {
         protected readonly CompositeDisposable CompositeDisposable = new();
+        [Inject] private DiContainer Container;
 
         [Inject]
         protected virtual void Initialize()
         {
             Observable.OnceApplicationQuit().Subscribe(_ => Terminate());
+        }
+
+        protected T Instantiate<T>(T prefab, Transform parent = null, bool worldPositionStays = true) where T : Component
+        {
+            var go = Container.InstantiatePrefabForComponent<T>(prefab, parent);
+
+            if (!worldPositionStays)
+            {
+                go.transform.localPosition = Vector3.zero;
+            }
+
+            Container.BindInstance(go);
+
+            return go;
         }
 
         private void Terminate()
