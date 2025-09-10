14dd1b6c0d1151ce8d7a0cbad8ea17e0447fb6a4
Yevhenii Ratushnyi <yevhenii.ratushnyi@gmail.com>
2025-09-10 14:20:31 +0300

    Update Input system

--- Core/Assets/Scripts/Services/Input/InputService.cs
+++ Core/Assets/Scripts/Services/Input/InputService.cs
@@ -9,32 +9,72 @@ namespace TendedTarsier.Core.Services.Input
     public class InputService : ServiceBase
     {
         private readonly GameplayInput _gameplayInput;
-        
-        public InputAction Aim;
-        public IObservable<InputAction.CallbackContext> OnAimStarted { get; private set; }
-        public IObservable<InputAction.CallbackContext> OnAimPerformed { get; private set; }
-        public IObservable<InputAction.CallbackContext> OnAimCanceled { get; private set; }
-        
-        public InputAction Movement;
-        public IObservable<InputAction.CallbackContext> OnMovementStarted { get; private set; }
-        public IObservable<InputAction.CallbackContext> OnMovementPerformed { get; private set; }
-        public IObservable<InputAction.CallbackContext> OnMovementCanceled { get; private set; }
-        
-        public IObservable<InputAction.CallbackContext> OnMenuButtonStarted { get; private set; }
-        public IObservable<InputAction.CallbackContext> OnMenuButtonPerformed { get; private set; }
-        public IObservable<InputAction.CallbackContext> OnMenuButtonCanceled { get; private set; }
 
-        public IObservable<InputAction.CallbackContext> OnMapButtonStarted { get; private set; }
-        public IObservable<InputAction.CallbackContext> OnMapButtonPerformed { get; private set; }
-        public IObservable<InputAction.CallbackContext> OnMapButtonCanceled { get; private set; }
+        /// Right stick/Mouse
+        public InputAction Look;
+        public IObservable<InputAction.CallbackContext> OnLookStarted { get; private set; }
+        public IObservable<InputAction.CallbackContext> OnLookPerformed { get; private set; }
+        public IObservable<InputAction.CallbackContext> OnLookCanceled { get; private set; }
 
-        public IObservable<InputAction.CallbackContext> OnEnterButtonStarted { get; private set; }
-        public IObservable<InputAction.CallbackContext> OnEnterButtonPerformed { get; private set; }
-        public IObservable<InputAction.CallbackContext> OnEnterButtonCanceled { get; private set; }
+        /// Left stick/WASD
+        public InputAction Move;
+        public IObservable<InputAction.CallbackContext> OnMoveStarted { get; private set; }
+        public IObservable<InputAction.CallbackContext> OnMovePerformed { get; private set; }
+        public IObservable<InputAction.CallbackContext> OnMoveCanceled { get; private set; }
 
-        public IObservable<InputAction.CallbackContext> OnOverviewButtonStarted { get; private set; }
-        public IObservable<InputAction.CallbackContext> OnOverviewButtonPerformed { get; private set; }
-        public IObservable<InputAction.CallbackContext> OnOverviewButtonCanceled { get; private set; }
+        /// Gamepad A/Space
+        public IObservable<InputAction.CallbackContext> OnJumpButtonStarted { get; private set; }
+
+        public IObservable<InputAction.CallbackContext> OnJumpButtonPerformed { get; private set; }
+        public IObservable<InputAction.CallbackContext> OnJumpButtonCanceled { get; private set; }
+
+        /// Gamepad B/Control
+        public IObservable<InputAction.CallbackContext> OnCrouchButtonStarted { get; private set; }
+
+        public IObservable<InputAction.CallbackContext> OnCrouchButtonPerformed { get; private set; }
+        public IObservable<InputAction.CallbackContext> OnCrouchButtonCanceled { get; private set; }
+
+        /// Gamepad X/Enter
+        public IObservable<InputAction.CallbackContext> OnAttackButtonStarted { get; private set; }
+
+        public IObservable<InputAction.CallbackContext> OnAttackButtonPerformed { get; private set; }
+        public IObservable<InputAction.CallbackContext> OnAttackButtonCanceled { get; private set; }
+
+        /// Gamepad Y/E
+        public IObservable<InputAction.CallbackContext> OnInteractButtonStarted { get; private set; }
+
+        public IObservable<InputAction.CallbackContext> OnInteractButtonPerformed { get; private set; }
+        public IObservable<InputAction.CallbackContext> OnInteractButtonCanceled { get; private set; }
+
+        /// Left stick click/Left Shift 
+        public IObservable<InputAction.CallbackContext> OnSprintButtonStarted { get; private set; }
+
+        public IObservable<InputAction.CallbackContext> OnSprintButtonPerformed { get; private set; }
+        public IObservable<InputAction.CallbackContext> OnSprintButtonCanceled { get; private set; }
+
+        /// D-Pad Left/Left arrow
+        public IObservable<InputAction.CallbackContext> OnLeftButtonStarted { get; private set; }
+
+        public IObservable<InputAction.CallbackContext> OnLeftButtonPerformed { get; private set; }
+        public IObservable<InputAction.CallbackContext> OnLeftButtonCanceled { get; private set; }
+
+        /// D-Pad Right/Right arrow
+        public IObservable<InputAction.CallbackContext> OnRightButtonStarted { get; private set; }
+
+        public IObservable<InputAction.CallbackContext> OnRightButtonPerformed { get; private set; }
+        public IObservable<InputAction.CallbackContext> OnRightButtonCanceled { get; private set; }
+
+        /// D-Pad Up/Up arrow
+        public IObservable<InputAction.CallbackContext> OnUpButtonStarted { get; private set; }
+
+        public IObservable<InputAction.CallbackContext> OnUpButtonPerformed { get; private set; }
+        public IObservable<InputAction.CallbackContext> OnUpButtonCanceled { get; private set; }
+
+        /// D-Pad Down/Down arrow
+        public IObservable<InputAction.CallbackContext> OnDownButtonStarted { get; private set; }
+
+        public IObservable<InputAction.CallbackContext> OnDownButtonPerformed { get; private set; }
+        public IObservable<InputAction.CallbackContext> OnDownButtonCanceled { get; private set; }
 
         public InputService(GameplayInput gameplayInput)
         {
@@ -50,16 +90,25 @@ namespace TendedTarsier.Core.Services.Input
 
         private void InitInput()
         {
-            Aim = _gameplayInput.Gameplay.Aim;
-            (OnAimStarted, OnAimPerformed, OnAimCanceled) = _gameplayInput.Gameplay.Aim.ToObservable();
-            Movement = _gameplayInput.Gameplay.Movement;
-            (OnMovementStarted, OnMovementPerformed, OnMovementCanceled) = _gameplayInput.Gameplay.Movement.ToObservable();
-            (OnMapButtonStarted, OnMapButtonPerformed, OnMapButtonCanceled) = _gameplayInput.Gameplay.Map.ToObservable();
-            (OnMenuButtonStarted, OnMenuButtonPerformed, OnMenuButtonCanceled) = _gameplayInput.Gameplay.Menu.ToObservable();
-            (OnEnterButtonStarted, OnEnterButtonPerformed, OnEnterButtonCanceled) = _gameplayInput.Gameplay.Enter.ToObservable();
-            (OnOverviewButtonStarted, OnOverviewButtonPerformed, OnOverviewButtonCanceled) = _gameplayInput.Gameplay.Overview.ToObservable();
+            Look = _gameplayInput.Player.Look;
+            (OnLookStarted, OnLookPerformed, OnLookCanceled) = _gameplayInput.Player.Look.ToObservable();
 
-            _gameplayInput.Gameplay.Enable();
+            Move = _gameplayInput.Player.Move;
+            (OnMoveStarted, OnMovePerformed, OnMoveCanceled) = _gameplayInput.Player.Move.ToObservable();
+
+            (OnJumpButtonStarted, OnJumpButtonPerformed, OnJumpButtonCanceled) = _gameplayInput.Player.Jump.ToObservable();
+            (OnCrouchButtonStarted, OnCrouchButtonPerformed, OnCrouchButtonCanceled) = _gameplayInput.Player.Crouch.ToObservable();
+            (OnAttackButtonStarted, OnAttackButtonPerformed, OnAttackButtonCanceled) = _gameplayInput.Player.Attack.ToObservable();
+            (OnInteractButtonStarted, OnInteractButtonPerformed, OnInteractButtonCanceled) = _gameplayInput.Player.Interact.ToObservable();
+
+            (OnSprintButtonStarted, OnSprintButtonPerformed, OnSprintButtonCanceled) = _gameplayInput.Player.Sprint.ToObservable();
+
+            (OnLeftButtonStarted, OnLeftButtonPerformed, OnLeftButtonCanceled) = _gameplayInput.Player.Left.ToObservable();
+            (OnRightButtonStarted, OnRightButtonPerformed, OnRightButtonCanceled) = _gameplayInput.Player.Right.ToObservable();
+            (OnUpButtonStarted, OnUpButtonPerformed, OnUpButtonCanceled) = _gameplayInput.Player.Up.ToObservable();
+            (OnDownButtonStarted, OnDownButtonPerformed, OnDownButtonCanceled) = _gameplayInput.Player.Down.ToObservable();
+
+            _gameplayInput.Player.Enable();
         }
     }
 }
